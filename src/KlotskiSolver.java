e62f9e2dbfffa8cdfdafd49546da54b0b8319a66
Pedro Miguel Sousa da Costa <up201605339@fe.up.pt>
2019-03-23 03:02:02 +0000

    added non working example: original

--- src/KlotskiSolver.java
+++ src/KlotskiSolver.java
@@ -7,6 +7,8 @@ public class KlotskiSolver {
 
     private int[][] starting_map = { { 1, 4, 4, 1 }, { 1, 4, 4, 1 }, { 2, 3, 3, 2 }, { 2, 1, 1, 2 }, { 1, 0, 0, 1 } };
 
+    private int[][] original_map = { { 2, 4, 4, 2 }, { 2, 4, 4, 2 }, { 2, 3, 3, 2 }, { 2, 1, 1, 2 }, { 1, 0, 0, 1 } };
+
     private int[][] starting_easy = { { 1, 4, 4, 1 }, { 1, 4, 4, 1 }, { 2, 1, 1, 2 }, { 2, 1, 1, 2 }, { 1, 0, 0, 1 } };
 
     private int[][] test_vertical = { { 0, 0, 0, 0 }, { 0, 2, 0, 0 }, { 2, 2, 0, 0 }, { 2, 0, 0, 0 }, { 0, 0, 0, 0 } };
