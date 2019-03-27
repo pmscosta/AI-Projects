282c3150800c8efd284b8ece397eef41a6f908c2
Pedro Costa <up201605339@fe.up.pt>
2019-03-27 00:27:20 +0000

    added length 2 moves

--- src/VerticalRectangle.java
+++ src/VerticalRectangle.java
@@ -4,6 +4,44 @@ public class VerticalRectangle {
 
         switch (e) {
 
+        case UP2: {
+
+            Block current = game.blocks.get(current_point);
+
+            if (current.x < 2 || current.x > 3 || current.y > 3 || current.y < 0)
+                break;
+
+            Point up_point = new Point(current.x - 1, current.y);
+
+            Block up = game.blocks.get(up_point);
+
+            Point up_point_2 = new Point(current.x - 2, current.y);
+
+            Block up_2 = game.blocks.get(up_point_2);
+
+            if (up == null || up_2 == null)
+                break;
+
+            // free space up
+            if (up.type == 0 && up_2.type == 0) {
+
+                current.x = current.x - 2;
+
+                game.blocks.replace(up_point, current);
+
+                game.blocks.replace(up_point_2, current);
+
+                game.blocks.replace(new Point(current.x + 3, current.y), up_2);
+
+                game.blocks.replace(current_point, up);
+
+                return new Klotski(game.constructMap());
+
+            }
+            break;
+
+        }
+
         case UP: {
 
             Block current = game.blocks.get(current_point);
@@ -31,6 +69,40 @@ public class VerticalRectangle {
 
             }
             break;
+        }
+
+        case DOWN2: {
+
+            Block current = game.blocks.get(current_point);
+
+            if (current.x > 1 || current.x < 0 || current.y < 0 || current.y > 3) {
+                break;
+            }
+
+            Point down_point = new Point(current.x + 2, current.y);
+            Block down = game.blocks.get(down_point);
+
+            Point down_point_2 = new Point(current.x + 3, current.y);
+            Block down_2 = game.blocks.get(down_point_2);
+
+            if (down == null || down_2 == null) {
+                break;
+            }
+            // free space down
+            if (down.type == 0 && down_2.type == 0) {
+
+                current.x = current.x + 2;
+
+                game.blocks.replace(current_point, down);
+                game.blocks.replace(new Point(current.x - 1, current.y), down_2);
+                game.blocks.replace(down_point, current);
+                game.blocks.replace(down_point_2, current);
+
+                return new Klotski(game.constructMap());
+            }
+
+            break;
+
         }
 
         case DOWN: {
